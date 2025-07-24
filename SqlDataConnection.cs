dd3370d0500e029db5ac3a64d16a6dc252c4938b
Zisa-dev <tanishatanjin7628@gmail.com>
2025-07-25 01:09:22 +0600

    Add files via upload

---
+++ SqlDataConnection.cs
@@ -0,0 +1,12 @@
+﻿namespace HR.UI
+{
+    internal class SqlDataConnection
+    {
+        private string connectionString;
+
+        public SqlDataConnection(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+    }
+}
